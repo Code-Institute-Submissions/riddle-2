e84a455543a7a0aa60e847c6ca95c7d51f3cb3a3
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-08-17 15:09:07 +0000

    Having issue with login and quiz question

--- app.py
+++ app.py
@@ -1,44 +1,49 @@
 import os
 from flask import Flask, render_template,flash, redirect, url_for, session, logging, request
 import json
-
+import random
 app = Flask(__name__)
-
 with open("data/quiz.json","r") as info:
             data = json.load(info)
-    
+counter_dict = {}
+
+
 @app.route('/',methods=['GET','POST'] )
 def login():
     if request.method == "POST": 
         with open("data/reg-detail.txt","r") as fp:  
-            valid_users = fp.readlines()
-            if request.form["username"] and request.form["password"]   == valid_users:
-                return redirect('/quiz')
+            valid_users = fp.read().splitlines()
+            username = request.form['username']
+            password = request.form['password']
+            if username + ':' + password == valid_users:
+                return "hi"
             else:
                 return redirect('/register')
-            
     return render_template('index.html')
+        
+    
 
     
 # quiz 
 @app.route('/quiz', methods=['GET','POST'])
 def quiz():
-    if request.method == "POST":
-        if data[0]["answer"] == request.form["Answer"]:
-            return "HI"
-        else:
-            return "Haha nice b"
-    return render_template('quiz.html', data=data)
+   
+    counter_dict = 0
+    if request.method == "POST" :
+         if data[counter_dict]["answer"] == (request.form["Answer"]):
+              counter_dict += 1
+            
+    return render_template('quiz.html', data=data ,i=counter_dict)
     
 #create an register form 
 @app.route('/register', methods=['GET','POST'])
 def register():
-        if request.method =="POST":
+     if request.method =="POST":
             with open("data/reg-detail.txt","a") as register_Detail:
-                register_Detail.writelines(request.form["username"] + "\t")
-                register_Detail.writelines(request.form["Password"] + "\n")
+                register_Detail.writelines(request.form["username"]+ "\t")
+                register_Detail.writelines(request.form["Password"]+ "\n")
                 return redirect('/quiz')
-        return render_template('register.html')  
+     return render_template('register.html')
 #create an login form 
 
 
