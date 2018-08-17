aa4f98a31ac7792d84bec773a36282342a78b313
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-08-17 19:12:05 +0000

    fixed the login error

--- app.py
+++ app.py
@@ -3,48 +3,61 @@ from flask import Flask, render_template,flash, redirect, url_for, session, logg
 import json
 import random
 app = Flask(__name__)
+
 with open("data/quiz.json","r") as info:
-            data = json.load(info)
-counter_dict = {}
+    data = json.load(info)
+print(data) #what you're comparing to
+    
+# counter_dict = {}
 
-
+#create an login form 
 @app.route('/',methods=['GET','POST'] )
 def login():
     if request.method == "POST": 
         with open("data/reg-detail.txt","r") as fp:  
             valid_users = fp.read().splitlines()
+            print(valid_users)
             username = request.form['username']
             password = request.form['password']
-            if username + ':' + password == valid_users:
-                return "hi"
+            if username + ':' + password in valid_users:
+                return redirect('/quiz')
             else:
                 return redirect('/register')
     return render_template('index.html')
         
     
 
-    
 # quiz 
 @app.route('/quiz', methods=['GET','POST'])
 def quiz():
-   
-    counter_dict = 0
-    if request.method == "POST" :
-         if data[counter_dict]["answer"] == (request.form["Answer"]):
-              counter_dict += 1
+    
+    counter = 0 # If it's a GET request we start the counter at 0
+    
+    if request.method == "POST":
+        counter = int(request.form["current_question_number"]) # Overwrite the counter if this is a post request
+        print(data[counter]["answer"], request.form["Answer"])
+        if data[counter]["answer"] == (request.form["Answer"]):
+            # Increment score
+            # increment the counter
+            # Possibly return a new variable for play_sound=True
+            # Handle what to do on the last question
+            print('correct!!!')
+            counter += 1
+        else:
+            print('WRONG!!!')
             
-    return render_template('quiz.html', data=data ,i=counter_dict)
+    return render_template('quiz.html', data=data, i=counter)
     
 #create an register form 
 @app.route('/register', methods=['GET','POST'])
 def register():
      if request.method =="POST":
             with open("data/reg-detail.txt","a") as register_Detail:
-                register_Detail.writelines(request.form["username"]+ "\t")
+                register_Detail.writelines(request.form["username"]+ ":")
                 register_Detail.writelines(request.form["Password"]+ "\n")
                 return redirect('/quiz')
      return render_template('register.html')
-#create an login form 
+
 
 
 app.run(host=os.getenv('IP'),
