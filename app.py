41da56029848bfa63dd23f31c7148ff5684eed8d
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-09-04 23:14:41 +0000

    testing the app.py file with unittest

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ import random
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
-with open("data/quiz.json","r") as info:
+with open("data/quiz.json","r") as info: # Opening the JSON file to get the questions and answers for my quiz 
     data = json.load(info)
 
-#create an login form 
+
 @app.route('/',methods=['GET','POST'] )
+#creating a function for login system
 def login():
     if request.method == "POST":
         with open("data/reg-detail.txt","r") as fp:  
@@ -18,7 +19,7 @@ def login():
             print(valid_users)
             session['username'] = request.form['username']
             password = request.form['password']
-            if session['username'] + ':' + password in valid_users:
+            if session['username'] + ':' + password in valid_users: 
                 return redirect(url_for("quiz"))
             else:
                 return redirect('/register')
@@ -27,11 +28,9 @@ def login():
     page_title="Go over |Home")
         
 
-
-# quiz 
 @app.route('/quiz', methods=['GET','POST'])
 def quiz():
-    session['counter'] = 0# If it's a GET request we start the counter at 0
+    session['counter'] = 0 # If it's a GET request we start the counter at 0
     session['score'] = 0
     session['turns'] = 0
     if request.method == "POST":
@@ -41,14 +40,14 @@ def quiz():
         # Overwrite the counter if this is a post request
         print(data[session['counter']]["answer"], request.form["Answer"])
         session['end'] = False
-        if session['counter'] == 11 or  session['end']:
+        if session['counter'] == 11 or  session['end']: # when session is end or question all finshed it will redirect to gameover page
             return redirect('/gameover')
         else:
              if data[session['counter']]["answer"].upper() == (request.form["Answer"]).upper():
                  flash('Right!')
                  session['counter'] += 1 
                  session['score'] += 1
-                 session['turns'] = 0
+                 session['turns'] = 0 # If answer is right then turns will get back to zero so when it run again it will start from 0 
                  print(session['turns'])
              elif data[session['counter']]["answer"].upper() != (request.form["Answer"]).upper():
                  session['turns'] += 1
@@ -56,13 +55,13 @@ def quiz():
                  print(session['turns'])
              if session['turns'] > 1:
                      flash('last question answer is ' + (data[session['counter']]["answer"]))
-                     session['turns'] = 0
+                     session['turns'] = 0 
                      session['counter'] += 1
     return render_template('quiz.html', 
     data=data,
-    i=session['counter'], 
-    s=session['score'],
-    t = session['turns'],
+    i=session['counter'], # 'i' stands for increment of question
+    s=session['score'],   # 's' stands for score 
+    t = session['turns'], # 't' satnds for turns  
     page_title="Quiz")
 
 def add_score_on_leaderboard(username, score):
@@ -78,8 +77,8 @@ def get_score():
             high_score.append(line)
         sorted_top_players = []
         for player in high_score:
-            tupe = (player.split(':')[0].strip(), int(player.split(':')[1].strip()))
-            sorted_top_players.append(tupe)
+            player_list = (player.split(':')[0].strip(), int(player.split(':')[1].strip()))
+            sorted_top_players.append(player_list)
 
         return sorted(sorted_top_players, key=lambda x: x[1])[::-1][:3]
 
--- test_app.py
+++ test_app.py
@@ -1,49 +1,31 @@
-
 import unittest
 import app
 app.testing = True
 class TestQuiz(unittest.TestCase):
-    def test_question(self):
-           self.assertEqual(1,1)
-    # tests:
-    # Before any of this, make sure you know how to make GET/POST requests
-    # Scores are being calculated properly
-    # Login returns a redirect
-    # Username exists in the session after logging in
-    # Test that riddle counter works properly
-    # Test that answer checking functionality works
-    # Test that username and password are correctly added to the register file
+    def test_the_test(self):
+        """
+        Just to make sure that our testing is working
+        """
+        self.assertEqual(1,1)
+   
+    def test_sort_high_score(self):
+        """
+        Test that our sorted is properly sorting the score in descending order 
+        """
+        result = app.get_score()
+        top_scores = [('Ram',10),('Priya',11),('ajil',7)]
+        self.assertNotEqual(result,top_scores)
     
-        
-       
-    def test_registration_works(self):
-          post_method = app.request.POST('/register') and app.register()
-          with open("data/reg-detail.txt","r") as register_Detail:
-                self.assertIn(post_method, register_Detail) 
-        #1. Make a POST to /register
-    #     2. Open the register_detail
-    #     3. Verify that register_detail has the username and password you POSTed in step 1
+    
+    def test_question_len(self):
+        """
+        Test to make sure that our 12 questions can be readable  
+        """
+        response = app.data
+        self.assertEqual(len(response),12)
         
     
-    # def test_counter(self):
-    #     session = {}
-    #     session['username'] = 'test_user'
-    #     session['score'] = 0
-    #     session['counter'] = 0
-    #     data = {}
-    #     data['session'] = session
-    #     url = '/quiz'
-    #     response = request.post(url, data=data)
-    #     print(response)
-    #def test_load_questions(self):
-    
-    # def test_login_returns_redirect(self):
-    #     pass
-    #     # test that login returns a redirect (status 302)
-        
-      
 print("Test passed")
     
-
 if __name__ == "__main__": 
       unittest.main() 
